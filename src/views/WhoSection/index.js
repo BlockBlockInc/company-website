15ad79553a9588c59dcd9b2c08d1ebbbe1984030
Carly Reilly <carly.reilly@tufts.edu>
2021-08-04 17:34:14 -0400

    Update index.js

--- src/views/WhoSection/index.js
+++ src/views/WhoSection/index.js
@@ -46,7 +46,7 @@ const members = [
   },
   {
     name: 'Angele',
-    img: "https://drive.google.com/file/d/1kA4fBJBWdkn7vHNYnRWTqGD_rQGdc3py/view?usp=sharing",
+    img: "https://drive.google.com/uc?export=view&id=1kA4fBJBWdkn7vHNYnRWTqGD_rQGdc3py",
     description: 'Intern. Angele is a Comp Sci & Stats undergrad student at McGill and recently got into the crypto/NFT space. As part of her internship with Block::Block, she is building a website that mints NFTs for social media influencers.',
   },
   {
