a153fbaa13c14313a122f0923e08a76101e453ef
Mark Mayo <mark@vmunix.com>
2021-08-04 20:33:36 -0700

    move avatar images to S3

--- src/views/WhoSection/index.js
+++ src/views/WhoSection/index.js
@@ -11,52 +11,52 @@ import "slick-carousel/slick/slick-theme.css";
 const members = [
    {
     name: 'Bart',
-    img: "https://drive.google.com/uc?export=view&id=1a3rtZENUbz2H1ggkv6oAI7CdZhZUffSz",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Bart.webp",
     description: 'Co-founder. Bart built 25 #1 App Store hits & ran smartphone games for Disney. He co-founded the Mozilla Builders incubator and headed marketing & business affairs for Firefox 1.0. He has a long track record in open source, community work and activism (incl as an Echoing Green fellow). Occasional angel investor (MUBI, Hooked).',
   },
   {
     name: 'Mark',
-    img: "https://drive.google.com/uc?export=view&id=1Z6BiGHyxDBm_zJY55A6JrPFwHx7fMJ7g",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Mark.webp",
     description: 'Co-founder. Mark was previously Chief Product Officer for Mozilla and steward of Firefox for the Quantum launch. He served as CTO for cloud computing pioneer Joyent (containers, Node.js), and has been the technical co-founder at multiple early startups.' 
   },
   {
     name: 'Kai',
-    img: "https://drive.google.com/uc?export=view&id=1ZLXMRo4AxRzUrijUOYzW59j3KNCpewrg",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Kai.webp",
     description: 'Advisor. Kai is in Experience Design at Netflix, where he works closely with content creators and product owners to create special experiences on the platform. He is the founder of the MeebitsDAO project and has spent years in the crypto world. He previously worked at Sony Pictures Entertainment.',
   },
   {
     name: 'Carly',
-    img: "https://drive.google.com/uc?export=view&id=1hYrgudK2BizMer6zf08sSfTwzaAdBEnh",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Carly.webp",
     description: 'Chief of Staff. Carly Reilly is an entrepreneur, political commentator, and fundraiser. Carly joined Andrew Yang’s 2020 presidential campaign in August 2018 and helped raise over $40 million as national finance director. She is a Washington Post contributor, a co-host of the Yang Speaks podcast, and has been featured by Bloomberg News.',
   },
     {
     name: 'Luke',
-    img: "https://drive.google.com/uc?export=view&id=12vxzpQGrSUmmvUstVHYARir2Z9lfPMEe",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Luke.webp",
     description: 'Creative Director. Luke has spent the last 20 years working as a creative at Wunderman, McCann, MRM, Publicis, and Digitas LBi, among other advertising giants. He was fortunate to spend two of those years traveling the world with Hyper Island, training advertising agencies on building agile management teams that solve for constant digital and social change.',
   },
   {
     name: 'Neo',
-    img: "https://drive.google.com/uc?export=view&id=1ONo4lgv0EzdtOUORVm7-oeydFYE2O1sr",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Neo.webp",
     description: 'Developer. Neo is currently taking a gap year from the University of Central Florida - where he studies computer science - to work with Block::Block. He enjoys building and iterating on new ideas and is excited about jumping into the world of Web3 and crypto. In his free time, he enjoys attending hackathons and meeting new people!',
   },
   {
     name: 'Mike',
-    img: "https://drive.google.com/uc?export=view&id=15SGYVN3c-bCxSpQTfVHFwndzkK6HFwr4",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Ginza.webp",
     description: 'Content & Community. Mike loves being at the breaking edge of culture, trends and consumer insight. He’s fascinated by the metaverse, NFT’s and the future of commerce. By day he is a senior strategist at MediaMonks advertising agency in London. By night you’ll find him in our Discords! You probably know him better as Ginza / GinzaETH and he’s all things content and community for BLOCK::BLOCK & MeebitsDAO.',  
   },
   {
     name: 'Angele',
-    img: "https://drive.google.com/uc?export=view&id=1WaNHNHoLTc6mqo3pZOcbNUL3bX2dwjkJ",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Angele.webp",
     description: 'Intern. Angele is a Comp Sci & Stats undergrad student at McGill and recently got into the crypto/NFT space. As part of her internship with Block::Block, she is building a website that mints NFTs for social media influencers.',
   },
   {
     name: 'Ryan',
-    img: "https://drive.google.com/uc?export=view&id=1qBXRu27xY5Z1G2gnYwTXyBLN1GVbqn57",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Ryan.webp",
     description: 'Technical Advisor. Ryan Ouyang is an engineer. A recovered hackathon addict with 10+ wins, Ryan has been a finalist at 3 ETHGlobal events and operated in and built blockchain apps for the biggest companies in Canada. He is currently working on a new NFT wearables system, as well as a new blockchain game.',
   },
   {
     name: 'Josh',
-    img: "https://drive.google.com/uc?export=view&id=160biDBcLWgzUB4o6QOoxAYm_h28b9YEL",
+    img: "https://blockblock-frontend.s3.us-west-2.amazonaws.com/avatars/Josh.webp",
     description: 'Advisor. Josh is a Web3 software engineer. He comes from a TradFi background, previously working at Capital One Labs (in-house product incubator & innovation lab) where he led crypto exploration efforts. He is now an active NFT and crypto enthusiast.',
   },
 ]
